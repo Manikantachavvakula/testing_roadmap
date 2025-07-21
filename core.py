2067ea979ef1745918584a9fc605527402ec7154
Manikantachavvakula <manikantachavvakula508@gmail.com>
2025-07-21 17:52:42 +0530

    Sets and tuples

--- core.py
+++ core.py
@@ -11,9 +11,36 @@ class solution():
             print(f"No {target} found in the list")
         n.pop()
         print(f"List after popping : {n}")
+        for i in n :
+            print (i)
+
+    def tuplee(self,n):
+        try :
+            n.append(2)
+        except AttributeError :
+            print("Tuples are immutable")
+
+    def setss(self,n):
+        b=set(n)
+        b.add(5)
+        print(f"Set after adding 5 : {b}")
+        if 4 in n :
+             b.remove(4)
+             print(f"Set after removing 4 : {b}")
+        else :
+            print("No 4 is found")
+        print(f"Set after no duplicates : {b} ")
+        for i in b :
+            print (i)
+
 
 a= solution()
 n = list(map(int,input("Enter list with space seperated : ").split()))
 target = int(input("Enter a number to remove : "))
 a.listss(n,target)
+
+b=tuple(map(int,input("Enter tuple : ").split()))
+a.tuplee(b)
         
+f = list(map(int,input('Enter Set : ').split()))
+a.setss(f)
