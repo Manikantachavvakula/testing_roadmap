9bc2c6f9677b4c4a5c6520ea4af9f03cf50a46a6
Manikantachavvakula <manikantachavvakula508@gmail.com>
2025-07-28 22:19:43 +0530

    wiats

---
+++ importlearn/file1.py
@@ -0,0 +1,14 @@
+class bhageera():
+    def addjetpack(self):
+        print("Added jet-pack")
+    def addbomb(self):
+        print("Added bomb")
+    def addguns(self):
+        print("Added guns")
+
+if __name__ == "__main__" :
+    print("The raise of bhageera")
+    a=bhageera()
+    a.addjetpack()
+    a.addbomb()
+    a.addguns()
---
+++ importlearn/file2.py
@@ -0,0 +1,18 @@
+from file1 import bhageera 
+
+class updates(): 
+    def updateJP(self):
+        print("Upgraded jet-packs")
+    def updatebomb(self):
+        print("Upgraded Bomb")
+    def updateguns(self):
+        print("updated guns")
+    
+if __name__ == '__main__':
+    print("The Phase 2 ")
+    b=bhageera()
+    b.addbomb()
+    a= updates()
+    a.updateJP()
+    a.updatebomb()
+    a.updateguns()
